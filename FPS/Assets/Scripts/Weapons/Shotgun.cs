55da22a88b25f0be4bd0a1a78f5113b35abf786e
jonter <jonterrusian@gmail.com>
2022-03-25 16:17:40 +0300

    Shotgun added

--- FPS/Assets/Scripts/Weapons/Shotgun.cs
+++ FPS/Assets/Scripts/Weapons/Shotgun.cs
@@ -3,18 +3,111 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Shotgun : MonoBehaviour
+public class Shotgun : Weapon
 {
+    bool isReloading = false;
 
-    void Shoot()
+    private void Start()
     {
-        Animator anim = GetComponent<Animator>();
+        ammoAllMax = 100;
+        ammoAll = 10;
+        maxAmmoInMagazine = 8;
+        currentAmmo = maxAmmoInMagazine;
+        StartCoroutine(PullWeapon());
+    }
+
+    protected override void HideWeaponAnim()
+    {
+        anim.SetTrigger("hide");
+    }
+
+    protected override void ShowWeaponAnim()
+    {
+        anim.SetTrigger("show");
+    }
+
+    private void Update()
+    {
+        if (!isActive) return;
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            PrepareShoot();
+        }
+
+        if (isInAction) return;
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            StartCoroutine(Reload());
+        }
+
+        UpdateUITexts();
+    }
+
+    void PrepareShoot()
+    {
+        if (isReloading)
+            StopReloading();
+
+        if (isInAction) return;
+
+        if (currentAmmo > 0)
+        {
+            
+            Shoot();
+            StartCoroutine(SetAction(1 / fireRate));
+        }
+        else
+        {
+            StartCoroutine(Reload());
+        }
+
+    }
+
+    public IEnumerator Reload()
+    {
+        if (ammoAll <= 0) yield break;
+        if (currentAmmo == maxAmmoInMagazine) yield break;
         anim.SetTrigger("reload");
+        isInAction = true;
+        yield return new WaitForSeconds(0.5f);
+        isReloading = true;
+        
+    }
+
+    void ReloadOneBullet()
+    {
+        currentAmmo++;
+        ammoAll--;
+        UpdateUITexts();
+
+        if(ammoAll <= 0 || currentAmmo == maxAmmoInMagazine)
+        {
+            StopReloading();
+        }
+    }
+
+    void StopReloading()
+    {
+        anim.SetTrigger("reload_end");
+        isReloading = false;
+        StartCoroutine(SetAction(0.5f));
+    }
+
+
+    protected override void Shoot()
+    {
+        base.Shoot();
+        currentAmmo--;
+        UpdateUITexts();
+        Animator anim = GetComponent<Animator>();
+        anim.SetTrigger("fire");
 
         AudioSource audio = GetComponent<AudioSource>();
         audio.Play();
 
-        //fireVFX.Play();
+        fireVFX.Play();
 
         for(int i = 0; i < 12; i++)
         {
